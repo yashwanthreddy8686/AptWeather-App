b2d2e0b6f839982cdcac6d8357f1de8fbc3b1b2c
Yashwanth reddy <yash868686reddy@gmail.com>
2022-06-07 14:32:43 -0400

    Done with Initial Dom functions setup

---
+++ dist/js/domFunctions.js
@@ -0,0 +1,24 @@
+export const addSpinner = (element) => {
+    animateButton(element);
+    setTimeout(animateButton, 1000, element)
+}
+
+const animateButton = (element) => {
+    element.classList.toggle('none');
+    element.nextElementSibling.classList.toggle('block')
+    element.nextElementSibling.classList.toggle('none')
+}
+
+export const displayError = (headerMsg, srMsg) => {
+    updateWeatherLocationHeader(headerMsg);
+    updateScreenReaderConfirmation(srMsg);
+}
+
+const updateWeatherLocationHeader = (message) => {
+    const h1 = document.querySelector('#currentForecast__location');
+    h1.textContent = message;
+}
+
+const updateScreenReaderConfirmation = (message) => {
+    document.querySelector('#confirmation').textContent = message;
+}
---
+++ dist/js/main.js
@@ -0,0 +1,43 @@
+import {
+    addSpinner,
+    displayError
+} from './domFunctions.js'
+import CurrentLocation from "./CurrentLocation.js"
+const currentLoc = new CurrentLocation();
+
+const initApp = () => {
+    // add listeners
+    const geoButton = document.querySelector('#getLocation');
+    geoButton.addEventListener('click', getGeoWeather)
+}
+
+document.addEventListener('DOMContentLoaded', initApp);
+
+const getGeoWeather = (event) => {
+    if (event) {
+        if (event.type === 'click') {
+            // add spinner
+            const mapIcon = document.querySelector('.fa-map-marker-alt');
+            addSpinner(mapIcon);
+        }
+    }
+    if (!navigator.geolocation) {
+        return geoError()
+    }
+    navigator.geolocation.getCurrentPosition(geoSuccess, geoError);
+}
+
+const geoError = (errObj) => {
+    const errMsg = errObj ? errObj.message : "Geolocation not supported";
+    displayError(errMsg, errMsg);
+}
+
+const geoSuccess = (position) => {
+    const myCoordsObj = {
+        lat: position.coords.latitude,
+        lon: position.coords.longitude,
+        name: `Lat:${position.coords.latitude} Long:${position.coords.longitude}`
+    };
+    // set location object
+    // update data and display 
+}
